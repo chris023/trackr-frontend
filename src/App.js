7aff0a068b0092fe2d066f98e4ebe0ead472ec69
chris023 <chris.meyer023@gmail.com>
2019-05-29 18:27:53 -0600

    Add default background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Switch, Redirect, Route } from 'react-router-dom'
+import { withStyles } from '@material-ui/core'
+
 import { Auth, Home } from './paths'
 import { AuthRoute } from './components'
 
-const App = () => {
+const styles = theme => ({
+  root: {
+    background: theme.palette.background.default,
+    minHeight: '100vh',
+    minWidth: '100vw',
+  },
+})
+
+const App = ({ classes }) => {
   return (
-    <Switch>
-      <Route exact path="/auth/login" component={Auth} />
-      <AuthRoute path="/home" component={Home} />
-      <Redirect to="/auth/login" />
-    </Switch>
+    <div className={classes.root}>
+      <Switch>
+        <Route exact path="/auth/login" component={Auth} />
+        <AuthRoute path="/home" component={Home} />
+        <Redirect to="/auth/login" />
+      </Switch>
+    </div>
   )
 }
 
-export default App
+App.propTypes = {
+  classes: PropTypes.object,
+}
+
+export default withStyles(styles)(App)
