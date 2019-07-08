1474c0843089a774a90106ad1b8e1a6a1655c23a
chris023 <chris.meyer023@gmail.com>
2019-07-08 16:04:30 -0600

    Render Map and UI

--- src/paths/Home/index.js
+++ src/paths/Home/index.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core'
 
-const Home = () => {
-  return <div>Hello</div>
+import { Map, UserInterface } from '../../components'
+
+const styles = () => ({
+  mapContainer: {
+    width: '100%',
+    height: 'calc(100vh - 64px)',
+  },
+})
+
+const Home = ({ classes }) => {
+  return (
+    <UserInterface>
+      <div className={classes.mapContainer}>
+        <Map />
+      </div>
+    </UserInterface>
+  )
 }
 
-export default Home
+Home.propTypes = {
+  classes: PropTypes.object,
+}
+
+export default withStyles(styles)(Home)
