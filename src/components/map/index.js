635aa9b7ffd04ca373aa5e6f2775be834062ab4e
chris023 <chris.meyer023@gmail.com>
2019-07-08 16:54:12 -0600

    Add Filters

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,23 +1,47 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import GoogleMapReact from 'google-map-react'
+import { withStyles } from '@material-ui/core'
 
 import Marker from './Marker'
+import Filters from './Filters'
 
-const myMap = () => {
+const styles = () => ({
+  root: {
+    height: '100%',
+    position: 'relative',
+    width: '100%',
+  },
+})
+
+const myMap = ({ classes, center, zoom }) => {
   return (
-    <GoogleMapReact
-      // eslint-disable-next-line no-undef
-      bootstrapURLKeys={{ key: process.env.REACT_APP_GMAPS_KEY }}
-      defaultCenter={{
-        lat: 30.25,
-        lng: -92,
-      }}
-      defaultZoom={10}
-    >
-      <Marker lat={30.5} lng={-92} />
-      <Marker lat={30.2} lng={-92.2} />
-    </GoogleMapReact>
+    <div className={classes.root}>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key:
+            process.env.NODE_ENV === 'development' &&
+            process.env.REACT_APP_GMAPS_KEY,
+        }}
+        defaultCenter={
+          center || {
+            lat: 30.25,
+            lng: -92,
+          }
+        }
+        defaultZoom={zoom || 10}
+      >
+        <Marker lat={30.5} lng={-92} />
+        <Marker lat={30.2} lng={-92.2} />
+      </GoogleMapReact>
+      <Filters />
+    </div>
   )
 }
 
-export default myMap
+myMap.propTypes = {
+  center: PropTypes.object,
+  zoom: PropTypes.number,
+}
+
+export default withStyles(styles)(myMap)
