011dd1ed79d35ffc2e8c3978efe5f3df39e0ec11
chris023 <chris.meyer023@gmail.com>
2019-07-08 16:04:45 -0600

    Update links

--- src/util/apollo/client.js
+++ src/util/apollo/client.js
@@ -1,14 +1,30 @@
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error'
+import { ApolloLink } from 'apollo-link'
 
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://crate-tracker-backend.herokuapp.com/graphql',
-  }),
-  cache: new InMemoryCache(),
+const cache = new InMemoryCache()
+
+const httpLink = new HttpLink({
+  uri: 'https://crate-tracker-backend.herokuapp.com/graphql',
 })
 
-const getClient = () => client
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    // do something with graphql error
+  }
 
-export default getClient
+  if (networkError) {
+    // do something with network error
+  }
+})
+
+const link = ApolloLink.from([errorLink, httpLink])
+
+const client = new ApolloClient({
+  link,
+  cache,
+})
+
+export default client
