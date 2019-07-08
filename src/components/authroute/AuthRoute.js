e3d8f2b8d93a57f681f63b4dc4eda44a7f906d1d
chris023 <chris.meyer023@gmail.com>
2019-07-08 16:03:08 -0600

    Check storage for token

--- src/components/authroute/AuthRoute.js
+++ src/components/authroute/AuthRoute.js
@@ -3,6 +3,10 @@ import { Route, Redirect } from 'react-router'
 import { connect } from 'react-redux'
 
 const AuthRoute = ({ component: Component, token, ...props }) => {
+  if (!token.length) {
+    const storage = sessionStorage.getItem('theoTracker')
+    if (storage) token = JSON.parse(storage).token || ''
+  }
   const isAuth = <Route {...props} component={Component} />
   const isNotAuth = <Redirect to="/auth/login" />
   return token.length ? isAuth : isNotAuth
