2bcca8944bf747a16640009978c1ec5021107fa4
chris023 <chris.meyer023@gmail.com>
2019-07-08 16:02:33 -0600

    Update to the way the Apollo client is provided

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -5,15 +7,15 @@ import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter } from 'react-router-dom'
 import { MuiThemeProvider } from '@material-ui/core'
 
-import getClient from './util/apollo/client'
-import theme from './util/material-ui/theme'
+import client from './util/apollo'
 import { store } from './util/redux'
+import theme from './util/material-ui/theme'
 import App from './App'
 
 import './index.css'
 
 ReactDOM.render(
-  <ApolloProvider client={getClient(store)}>
+  <ApolloProvider client={client}>
     <Provider store={store}>
       <BrowserRouter basename="/app">
         <MuiThemeProvider theme={theme}>
