c9fc6383eb8ed96d2b506f50eff9d30d609c356c
chris023 <chris.meyer023@gmail.com>
2019-07-24 12:52:12 -0600

    Add Files

--- src/paths/index.js
+++ src/paths/index.js
@@ -1,6 +1,7 @@
 import Assets from './Assets'
 import Auth from './Auth'
+import Files from './Files'
 import Home from './Home'
 import Trackers from './Trackers'
 
-export { Assets, Auth, Home, Trackers }
+export { Assets, Auth, Files, Home, Trackers }
