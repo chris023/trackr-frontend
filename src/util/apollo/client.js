afd5e6062493eab7606b0b8378877372c9aec0ab
chris023 <chris.meyer023@gmail.com>
2019-07-24 14:27:40 -0600

    Add node_env log

--- src/util/apollo/client.js
+++ src/util/apollo/client.js
@@ -9,6 +9,7 @@ const cache = new InMemoryCache()
 // const uri = process.env.NODE_ENV === 'development'
 //   ? 'http://localhost:8000/graphql'
 //   : 'https://crate-tracker-backend.herokuapp.com/graphql'
+console.log(process.env.NODE_ENV)
 const uri = 'https://crate-tracker-backend.herokuapp.com/graphql'
 
 const uploadLink = createUploadLink({ uri })
